910ede4c81055ff572377c07327ad85e3c9f3898
Daniel Toader & Cynthina Wilhelm <student@epicodus.com>
2015-04-20 14:29:25 -0700

    add reviews

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,8 +1,10 @@
 restaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
   $scope.restaurants = [];
   $scope.addRestaurant = function() {
-    $scope.restaurants.push({name: $scope.restaurantName});
+    $scope.restaurants.push( { name: $scope.restaurantName, type: $scope.restaurantType, location: $scope.restaurantLocation, proper: "not reviewed" } );
     $scope.restaurantName = null;
+    $scope.restaurantType = null;
+    $scope.restaurantLocation = null;
   };
   $scope.deleteRestaurant = function(restaurant) {
     var index = $scope.restaurants.indexOf(restaurant);
